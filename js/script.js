c729813208d01e5d8833c2a51228e2eeb75f5152
unknown <kola.alda7@gmail.com>
2022-05-12 21:57:47 +0200

    website1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let loginFrom = document.querySelector('.header .login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginFrom.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginFrom.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    loginFrom.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+}
